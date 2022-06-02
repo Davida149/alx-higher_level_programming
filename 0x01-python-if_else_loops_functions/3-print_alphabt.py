9f924ae042dbf9a3b78ac3e16b98f5d8ff4bae1f
Davida149 <adegundedavid17@gmail.com>
2022-06-02 02:02:18 +0100

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for char in range(26):
     if (char == 4) and (char == 16):
         break
     else:
-        print("{:s}".format(char(char + ord("a"))))
+        print("{:s}".format(chr(char + ord("a"))))
